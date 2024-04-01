98ef92c42e0a924c65104d0ee47cdc31aaeb0740
carlandreiborromeo <borromeocarlandrei1@gmail.com>
2024-04-01 13:06:51 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        const int SecondsInMinute = 60;
+        const int MinutesInHour = 60;
+        const int HoursInDay = 24;
+        const int DaysInMonth = 30; 
+
+        
+        int seconds = 86400; 
+
+        
+        double minutes = seconds / (double)SecondsInMinute;
+
+        
+        double hours = minutes / MinutesInHour;
+
+       
+        double days = hours / HoursInDay;
+
+        
+        double months = days / DaysInMonth;
+
+     
+        Console.WriteLine($"{seconds} seconds is equal to:");
+        Console.WriteLine($"{minutes} minutes");
+        Console.WriteLine($"{hours} hours");
+        Console.WriteLine($"{days} days");
+        Console.WriteLine($"{months} months (approximately)");
+
+        Console.ReadLine(); 
+    }
+}
