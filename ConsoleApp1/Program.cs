e04c919d0820bfed396658eaec42dd274ff5754f
carlandreiborromeo <164586077+carlandreiborromeo@users.noreply.github.com>
2024-04-02 09:18:26 +0800

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,37 +1,26 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
         const int SecondsInMinute = 60;
         const int MinutesInHour = 60;
         const int HoursInDay = 24;
-        const int DaysInMonth = 30; 
+        const int DaysInMonth = 30;
 
-        
-        int seconds = 86400; 
+        int seconds = 86400;
 
-        
         double minutes = seconds / (double)SecondsInMinute;
-
-        
         double hours = minutes / MinutesInHour;
-
-       
         double days = hours / HoursInDay;
+        double months = DaysInMonth / days;
 
-        
-        double months = days / DaysInMonth;
-
-     
         Console.WriteLine($"{seconds} seconds is equal to:");
         Console.WriteLine($"{minutes} minutes");
         Console.WriteLine($"{hours} hours");
         Console.WriteLine($"{days} days");
-        Console.WriteLine($"{months} months (approximately)");
-
-        Console.ReadLine(); 
+        Console.WriteLine($"{months} months");
+        Console.ReadLine();
     }
 }
